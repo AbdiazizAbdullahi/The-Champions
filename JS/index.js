e734a9b7a99aa234002b634455a3aacea46b58a7
AbdiazizAbdullahi <taqiyudinabdullahi2001@gmail.com>
2022-12-16 19:40:57 +0300

    db.json file was edited

--- JS/index.js
+++ JS/index.js
@@ -1,33 +1,60 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     function fetchData(){
-        fetch('')
+        fetch('http://localhost:3000/players')
         .then(res => res.json())
-        .then(data => renderPlayers(data))
+        .then(data => renderPlayers(data));
     }
 
-    function renderPlayers(){
-        const li = document.querySelectorAll('li');
-        const a = document.querySelectorAll('a');
+    function renderPlayers(data){
+        const body = document.body
+        const li2 = document.getElementById('players-list');
+        const blog = document.getElementById('blog');
+        const video = document.getElementById('video');
+        const playerFrame = document.createElement('div');
+        playerFrame.classList.add('frame');
 
 
-        data.forEach(player => {
+
+        data.forEach( player => {
         const playerCard = document.createElement('div');
         playerCard.classList.add('player-card');
         playerCard.innerHTML = `
            <img class="player-image" src="${player.Image}"/>
-           <h3 class="player-name">${player.Name}</h3>
-           <h5 class="player-age">Age: ${player.Age}</h5>
-           <h5 class="player-position">Position${player.Position}</h5>
-           <h5 class="player-goals">Goals: ${player.Goals}</h5> 
-           <h5 class="player-assist">Assist: ${player.Assist}</h5>
+           <h3 class="player-name font">${player.Name}</h3>
+           <h5 class="player-age font">Age: ${player.Age}</h5>
+           <h5 class="player-position font">Position: ${player.Position}</h5>
+           <h5 class="player-goals font">Goals: ${player.Goals}</h5> 
+           <h5 class="player-assist font">Assist: ${player.Assist}</h5>
         `
+        playerFrame.appendChild(playerCard)
+
         
 
+        li2.addEventListener('click', () => {
+            blog.remove();
+            video.remove();
+            li2.appendChild(playerFrame);
+            body.appendChild(li2)
+        })
+
+        
+
+
         });
+
+        
+
+
         
 
     }
+
+
+
+
+
+    fetchData()
     
 
 
